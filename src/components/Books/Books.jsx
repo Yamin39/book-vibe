35cf9ff54fe3c4f088935cf91d88bb75127d0a6e
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-03-26 14:39:20 +0600

    added json data with images

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -4,7 +4,7 @@ const Books = () => {
   return (
     <div>
       <div className="text-center">
-        <h2 className="text-5xl font-bold font-playfair-display mb-12">Books</h2>
+        <h2 className="text-[2.5rem] font-bold font-playfair-display mb-12 text-dark">Books</h2>
       </div>
       <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
         <Book></Book>
