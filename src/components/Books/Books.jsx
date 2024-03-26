81e9d8fa9ec81f560f493de514b7bf4ff6975244
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-03-26 15:37:51 +0600

    added book cards dynamically

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
 import Book from "../Book/Book";
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+
   return (
     <div>
       <div className="text-center">
         <h2 className="text-[2.5rem] font-bold font-playfair-display mb-12 text-dark">Books</h2>
       </div>
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
-        <Book></Book>
-        <Book></Book>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center content-center">
+        {books.map((book) => (
+          <Book key={book.bookId} book={book}></Book>
+        ))}
       </div>
     </div>
   );
