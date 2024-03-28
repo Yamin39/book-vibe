44d47a3445fe5d8a233db28a3d1c638bfe7d6a36
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-03-28 14:38:22 +0600

    updated charts and json data

--- src/pages/PagesToRead/PagesToRead.jsx
+++ src/pages/PagesToRead/PagesToRead.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { Bar, BarChart, CartesianGrid, Cell, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, Cell, ResponsiveContainer, Text, Tooltip, XAxis, YAxis } from "recharts";
 import { getStoredReadBooks } from "../../utilities/readList";
 
 const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
@@ -28,37 +28,56 @@ const PagesToRead = () => {
   }, [books]);
 
   return (
-    <div className="py-10 px-2 sm:px-4 bg-[#13131308] rounded-3xl">
-      <ResponsiveContainer aspect={3}>
-        <BarChart
-          // width={}
-          height={600}
-          data={readListBooks}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <Tooltip></Tooltip>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="bookName" />
-          {/* interval={0} */}
-          <YAxis />
-          <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: "top" }}>
-            {readListBooks.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index % 20]} />)}
-          </Bar>
-        </BarChart>
-      </ResponsiveContainer>
+    <div className="h-screen sm:h-auto">
+      <div className="py-10 h-min sm:h-fit px-2 sm:px-4 bg-[#13131308] rounded-3xl">
+        <ResponsiveContainer aspect={2}>
+          <BarChart
+            data={readListBooks}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 40,
+            }}
+          >
+            <Tooltip></Tooltip>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis className="text-[0.5rem] sm:text-xs" dataKey="bookName" interval={0} tick={<CustomXAxisTick width={(400 - 80) / 7} />} />
+            <YAxis />
+            <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: "top" }}>
+              {readListBooks.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
+};
+
+const CustomXAxisTick = ({ x, y, payload, width }) => {
+  if (payload?.value) {
+    return (
+      <Text width={width} x={x} y={y} textAnchor="middle" verticalAnchor="start">
+        {payload.value}
+      </Text>
+    );
+  }
+  return null;
 };
 
 TriangleBar.propTypes = {
   height: PropTypes.number,
   width: PropTypes.number,
   fill: PropTypes.string,
+  x: PropTypes.number,
+  y: PropTypes.number,
+};
+
+CustomXAxisTick.propTypes = {
+  width: PropTypes.number,
+  payload: PropTypes.object,
   x: PropTypes.number,
   y: PropTypes.number,
 };
